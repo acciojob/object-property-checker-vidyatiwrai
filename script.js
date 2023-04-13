516559c4fc94a60b07904b23766b3c4da9a339ec
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-13 15:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
-function hasKey(key) {
   //   write your code here
+	
+function hasKey(key) {
+return key in sampleObject;
 }
 
 // Do not change the code below
